ea8fd7ebdd5cf94c7bc289db02fb09f1cb0d690c
IacuboiAnna <70690332+IacuboiAnna@users.noreply.github.com>
2020-11-27 19:53:43 +0200

    Add files via upload

---
+++ Problema_4.py
@@ -0,0 +1,7 @@
+#Introduceti cuvinte formate din minim 3 litere!
+a=input("Inrtoduceti primul cuvant:")
+b=input("Inrtoduceti al doilea cuvant:")
+c=input("Inrtoduceti al treilea cuvant:")
+d=input("Inrtoduceti al patrulea cuvant:")
+n=len(d)
+print(a[0:2]+b[0]+c[0:3]+d[0:(n//2)])
